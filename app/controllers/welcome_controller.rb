5713b149ea4fa6c30dd61401903a5fd5e3fd53f3
Tom Harpur <tom.harpur@earlymarket.com>
2019-10-18 19:15:09 -0700

    showing outlets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @user = User.find_by(id: params[:id])
     @offers = @user ? @user.preferred_offers : Offer.all
+    @outlets = Outlet.all
   end
 end
