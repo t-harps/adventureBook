d7004eada9f7bc08c7af122acb1d4debae1e9aee
Tom Harpur <tom.harpur@earlymarket.com>
2019-10-19 14:21:54 -0700

    making sure site works with no data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @user = User.find_by(id: params[:id])
     @offers = @user ? @user.preferred_offers : Offer.all
+    @first = @offers.exists? ? @offers.first : Placeholder.new
     @outlets = Outlet.all
   end
 end
---
+++ app/models/placeholder.rb
@@ -0,0 +1,17 @@
+class Placeholder
+  def activity_type
+    Placeholder.new
+  end
+
+  def image
+    "Surf"
+  end
+
+  def name
+    "Placeholder"
+  end
+
+  def description
+    "Placeholder"
+  end
+end
